21baced330947a8728bb03e75d58fe008bced94c
Mohit Singh <mohitsi2@cisco.com>
2021-03-27 15:40:00 +0530

    Add project files

---
+++ app/src/main/java/net/penguincoders/bmicalculator/MainActivity.kt
@@ -0,0 +1,75 @@
+package net.penguincoders.bmicalculator
+
+import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val imageBoy = findViewById<ImageView>(R.id.image_boy)
+        val imageGirl = findViewById<ImageView>(R.id.image_girl)
+        val weight = findViewById<EditText>(R.id.weight_value)
+        val height = findViewById<EditText>(R.id.height_value)
+        val calculateButton = findViewById<Button>(R.id.calculate_button)
+        val bmi = findViewById<TextView>(R.id.bmi)
+        val bmiStatus = findViewById<TextView>(R.id.bmi_status)
+        val bmiView = findViewById<LinearLayout>(R.id.bmiView)
+        val calculateAgainButton = findViewById<TextView>(R.id.calculate_again)
+
+        imageBoy.setOnClickListener {
+            imageBoy.setImageResource(R.drawable.ic_boy)
+            imageGirl.setImageResource(R.drawable.ic_girl_blur)
+        }
+
+        imageGirl.setOnClickListener {
+            imageBoy.setImageResource(R.drawable.ic_boy_blur)
+            imageGirl.setImageResource(R.drawable.ic_girl)
+        }
+
+        calculateButton.setOnClickListener {
+            var weightValue = 0.0
+            var heightValue = 0.0
+            if (weight.text.toString().isNotEmpty()) {
+                weightValue = weight.text.toString().toDouble()
+            }
+            if (height.text.toString().isNotEmpty()) {
+                heightValue = (height.text.toString().toDouble() / 100)
+            }
+            if (weightValue > 0.0 && heightValue > 0.0) {
+                val bmiValue = String.format("%.2f", weightValue / heightValue.pow(2))
+                bmi.text = bmiValue
+                bmiStatus.text = bmiStatusValue(weightValue / heightValue.pow(2))
+                bmiView.visibility = VISIBLE
+                calculateButton.visibility = GONE
+            } else
+                Toast.makeText(this, "Please Input Weight and Height Values greater than 0", Toast.LENGTH_LONG).show()
+        }
+
+        calculateAgainButton.setOnClickListener {
+            bmiView.visibility = GONE
+            calculateButton.visibility = VISIBLE
+            weight.text.clear()
+            height.text.clear()
+            weight.requestFocus()
+        }
+    }
+
+    private fun bmiStatusValue(bmi: Double): String {
+        lateinit var bmiStatus: String
+        if (bmi < 18.5)
+            bmiStatus = "Underweight"
+        else if (bmi >= 18.5 && bmi < 25)
+            bmiStatus = "Normal"
+        else if (bmi >= 25 && bmi < 30)
+            bmiStatus = "Overweight"
+        else if (bmi > 30)
+            bmiStatus = "Obese"
+        return bmiStatus
+    }
+}
